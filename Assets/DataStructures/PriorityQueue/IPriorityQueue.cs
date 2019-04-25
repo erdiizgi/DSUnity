7b0622914f654334a6b8eb1d0d03d553caa80c3c
Erdi İzgi <erdiizgi@gmail.com>
2019-04-25 18:46:43 +0200

    Add first two data structures for Unity: Fibonacci Heap and Priority Queue
    * Introduce .git files

---
+++ Assets/DataStructures/PriorityQueue/IPriorityQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataStructures.PriorityQueue
+{
+    public interface IPriorityQueue<T, in TKey> where TKey : IComparable<TKey>
+    {
+        /// <summary>
+        /// Inserts and item with a priority
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="priority"></param>
+        void Insert(T item, TKey priority);
+
+        /// <returns>The element with the highest priority</returns>
+        T Top();
+
+        /// <summary>
+        /// Deletes the element with the highest priority
+        /// </summary>
+        /// <returns>The deleted element</returns>
+        T Pop();
+    }
+}
---
+++ Assets/DataStructures/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DataStructures.FibonacciHeap;
+
+namespace DataStructures.PriorityQueue
+{
+    public class PriorityQueue<TElement, TPriority> : IPriorityQueue<TElement, TPriority>
+        where TPriority : IComparable<TPriority>
+    {
+        private readonly FibonacciHeap<TElement, TPriority> heap;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="minPriority">Minimum value of the priority - to be used for comparing.</param>
+        public PriorityQueue(TPriority minPriority)
+        {
+            heap = new FibonacciHeap<TElement, TPriority>(minPriority);
+        }
+
+        public void Insert(TElement item, TPriority priority)
+        {
+            heap.Insert(new FibonacciHeapNode<TElement, TPriority>(item, priority));
+        }
+
+        public TElement Top()
+        {
+            return heap.Min().Data;
+        }
+
+        public TElement Pop()
+        {
+            return heap.RemoveMin().Data;
+        }
+    }
+}
+
+
