3c6e43f991c065fd51f670dc887480ba1f13dd7c
Erdi İzgi <erdiizgi@gmail.com>
2019-10-16 19:38:32 +0200

    Add BiDictionary data type and its simple unity tests
    * Modify the structure a bit so the project will be more maintainable
    * Add unit test support
    * Clean up the package dependencies

---
+++ Assets/DataStructures/BiDictionary/IBiDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.BiDictionary
+{
+    public interface IBiDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        Dictionary<TKey, TValue> KeyMap { get; }
+        Dictionary<TValue, TKey> ValueMap { get; }
+
+        void Add(TKey key, TValue value);
+        void Clear();
+        bool ContainsKey(TKey key);
+        bool ContainsValue(TValue value);
+        int Count();
+        new IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();
+        IEnumerator<KeyValuePair<TValue, TKey>> GetValueEnumerator();
+        bool RemoveKey(TKey key);
+        bool RemoveValue(TValue value);
+    }
+}
